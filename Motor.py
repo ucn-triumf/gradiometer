0b632cfd216fb0c5dd7aceba25fcb8dc2cdfde7c
Cole Bunch <bunchcole@gmail.com>
2020-02-28 18:37:31 -0800

    figured out proper import statements

--- Motor.py
+++ Motor.py
@@ -1,11 +1,15 @@
 #MOTOR
 
 import atexit
+from Adafruit_Motor_HAT_Python_Library.Adafruit_MotorHAT.Adafruit_MotorHAT_Motors import Adafruit_MotorHAT, Adafruit_StepperMotor
+
 
 class Motor:
     def __init__(self):
+        self.mh = Adafruit_MotorHAT()
+        self.myStepper = self.mh.getStepper(200, 1)
 
     def turnOffMotors(self):
         self.mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
 	
-    atexit.register(turnOffMotors)
+        atexit.register(turnOffMotors)
