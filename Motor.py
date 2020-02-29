dca9f85a744dad186c93f10a82f0d7ff4a6820f9
Cole Bunch <bunchcole@gmail.com>
2020-02-28 16:08:50 -0800

    commit test

--- Motor.py
+++ Motor.py
@@ -1,1 +1,11 @@
 #MOTOR
+
+import atexit
+
+class Motor:
+    def __init__(self):
+
+    def turnOffMotors(self):
+        self.mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+	
+    atexit.register(turnOffMotors)
