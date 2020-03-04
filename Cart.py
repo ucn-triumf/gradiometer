bfc0813bb36eb51d11f45fda9cbd4764b698295b
Cole Bunch <bunchcole@gmail.com>
2020-03-04 12:47:11 -0800

    absorbed Cart into Gradiometer

--- Cart.py
+++ Cart.py
@@ -11,11 +11,26 @@ class Cart:
     
     def loadPos(self):
         posFile = open('POSITION.pickle','rb')
-        pos = pickle.load(posFile)
+        try:
+            pos = pickle.load(posFile)
+        except EOFError:
+            self.zero()
+            self.savePos()
+            self.loadPos()
         posFile.close()
         return pos
     
     def savePos(self):
         posFile = open('POSITION.pickle','wb')
-        pickle.dump(self.pos)
+        pickle.dump(self.pos, posFile)
         posFile.close()
+        print('saved pos')
+
+    def zero(self):
+        self.setPos(0)
+    
+    def setPos(self,x):
+        self.pos = x
+
+    def getPos(self):
+        return self.pos
--- Gradiometer.py
+++ Gradiometer.py
@@ -1,20 +1,47 @@
 #GRADIOMETER
 
 import atexit
+import pickle
 
 from Motor import Motor
-from Cart import Cart
 
 class Gradiometer:
 
     def __init__(self):
         
         self.motor = Motor()
-        self.cart = Cart(self.motor)
+        self.pos = self.loadPos()
+    
+    def loadPos(self):
+        posFile = open('POSITION.pickle','rb')
+        try:
+            pos = pickle.load(posFile)
+        except EOFError:
+            self.zero()
+            self.savePos()
+            self.loadPos()
+        posFile.close()
+        return pos
+    
+    def savePos(self):
+        posFile = open('POSITION.pickle','wb')
+        pickle.dump(self.pos, posFile)
+        posFile.close()
+        print('saved pos')
+
+    def zero(self):
+        self.setPos(0)
+    
+    def setPos(self,x):
+        self.pos = x
+
+    def getPos(self):
+        return self.pos
 
 def main():
     gradiometer = Gradiometer()
     atexit.register(gradiometer.motor.turnOffMotors)
+    atexit.register(gradiometer.savePos)
 
 if __name__ == '__main__':
     main()
