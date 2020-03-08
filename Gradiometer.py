0c6556afcac4450ca7bc4e774c8acdb5cb3d4117
Cole Bunch <bunchcole@gmail.com>
2020-03-08 15:06:24 -0700

    added option for running the gradiometer over a period of time rather than as a function of position

--- Gradiometer.py
+++ Gradiometer.py
@@ -1,5 +1,7 @@
 #GRADIOMETER
 
+import sys
+import traceback
 import atexit
 import pickle
 import numpy as np
@@ -80,7 +82,7 @@ class Gradiometer:
     def calibration(self):
         self.motor.myStepper.step(1000, self.motor.mh.FORWARD, self.motor.mh.DOUBLE)
     
-    def run(self,start,stop,tag):
+    def posRun(self,start,stop,tag):
         filename = 'Run_Data/{}-{}.csv'.format(datetime.now().strftime('%Y-%m-%d_%H-%M-%S'),tag)
         csvfile = open(filename, 'w')
         fieldnames = ['time','position','x1','y1','z1','x2','y2','z2']
@@ -110,12 +112,92 @@ class Gradiometer:
         print('finished at {}cm'.format(self.pos))
         self.motor.turnOffMotors()
 
+    def timeRun(self,cm,sec,tag):
+        filename = 'Run_Data/{}-{}.csv'.format(datetime.now().strftime('%Y-%m-%d_%H-%M-%S'),tag)
+        csvfile = open(filename, 'w')
+        fieldnames = ['time','position','x1','y1','z1','x2','y2','z2']
+        writer = csv.DictWriter(csvfile,fieldnames)
+        writer.writeheader()
+
+        ainchannels = range(6)
+        channeloptions = [0] * 6
+        maxrequests = 100
+        scanfreq 	= 1000
+        self.labjack.getCalibrationData()
+        self.labjack.streamConfig(NumChannels=len(ainchannels),ResolutionIndex=1,SettlingFactor=0,ChannelNumbers=ainchannels,ChannelOptions=channeloptions,ScanFrequency=scanfreq)
+
+        missed = 0
+        dataCount = 0
+        packetCount = 0
+
+        self.goTo(cm)
+        print('starting run at {}cm'.format(self.pos))
+
+        try:
+            self.labjack.streamStart()
+            start = datetime.now()
+            print('starting run at {}'.format(start))
+
+            for r in self.labjack.streamData():
+                if r is not None:
+                    #stop condition
+                    if (datetime.now()-start).seconds > sec:
+                        break
+                    if r['errors'] != 0:
+                        print("Error: %s ; " % r['errors'], datetime.now())
+                    if r['numPackets'] != self.labjack.packetsPerRequest:
+                        print("----- UNDERFLOW : %s : " % r['numPackets'], datetime.now())
+                    if r['missed'] != 0:
+                        missed += r['missed']
+                        print("+++ Missed ", r['missed'])
+                    
+                    time = datetime.now()
+                    x1 = r['AIN0']
+                    y1 = r['AIN1']
+                    z1 = r['AIN2']
+                    x2 = r['AIN3']
+                    y2 = r['AIN4']
+                    z2 = r['AIN5']
+
+                    x1val = sum(x1)/len(x1)
+                    y1val = sum(y1)/len(y1)
+                    z1val = sum(z1)/len(z1)
+                    x2val = sum(x2)/len(x2)
+                    y2val = sum(y2)/len(y2)
+                    z2val = sum(z2)/len(z2)
+
+                    print('measuring at {}, x1={:2.3f} y1={:2.3f} z1={:2.3f}, x2={:2.3f} y2={:2.3f} z2={:2.3f}'.format(time.strftime('%Y-%m-%d_%H-%M-%S'),x1val,y1val,z1val,x2val,y2val,z2val))
+                    writer.writerow({'time':time,'position':cm,'x1':x1val,'y1':y1val,'z1':z1val,'x2':x2val,'y2':y2val,'z2':z2val})
+
+                    dataCount += 1
+                    packetCount += r['numPackets']
+                
+                else:
+                    print('no data')
+        
+        except Exception as e:
+            tb = sys.exc_info()[-1]
+            print(traceback.extract_tb(tb, limit=1)[-1][1]) # Print what line the Exception occured on
+            print(e) #  Print the exception
+        finally:
+            stop = datetime.now()
+            self.labjack.streamStop()
+            #self.labjack.close()
+            print('ending run at {}'.format(stop))
+            sampleTotal = packetCount * self.labjack.streamSamplesPerPacket
+            scanTotal = sampleTotal / len(ainchannels)
+            print("{} requests with {} packets per request with {} samples per packet = {} samples total.".format(dataCount, (float(packetCount)/dataCount), self.labjack.streamSamplesPerPacket, sampleTotal))
+            print("{} samples were lost due to errors.".format(missed))
+            scanTotal -= missed
+            print ("Adjusted total: {}".format(scanTotal))
+
+
 def main():
     gradiometer = Gradiometer()
     atexit.register(gradiometer.motor.turnOffMotors)
     atexit.register(gradiometer.savePos)
 
-    gradiometer.run(0,10,'runtest')
+    gradiometer.posRun(0,10,'runtest')
 
 if __name__ == '__main__':
     main()
