5d1d338dfa7b549608dac71fe3bce6a5e65de12d
Cole Bunch <bunchcole@gmail.com>
2020-03-20 16:56:01 -0700

    added a routine for radial shielding measurements

---
+++ radial_coil_routine.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 16:47:55 2020
+
+@author: bunch
+"""
+
+from Gradiometer import Gradiometer
+import numpy as np
+import atexit
+
+g=Gradiometer()
+atexit.register(g.motor.turnOffMotors)
+atexit.register(g.savePos)
+g.zero()
+positions = np.linspace(14,64,51)
+for pos in positions:
+    input('set to {}cm and continue'.format(pos))
+    g.timeRun(10,'radial-shield-{}'.format(pos),pos,False)
